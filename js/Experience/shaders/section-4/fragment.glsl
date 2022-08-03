fba1babba0b22039f473231d21b591bb762ad146
Alex Di Guida <alexandre.diguida@gmail.com>
2022-08-03 12:09:34 +0200

    Step, mix, smoothstep

--- js/Experience/shaders/section-4/fragment.glsl
+++ js/Experience/shaders/section-4/fragment.glsl
@@ -1,5 +1,5 @@
 
-varying vec2 vUvs;
+varying vec2 vUv;
 
 uniform vec2 resolution;
 
@@ -12,5 +12,49 @@ vec3 yellow = vec3(1.0, 1.0, 0.0);
 void main() {
   vec3 colour = vec3(0.0);
 
+  float value1 = vUv.x;
+  float value2 = smoothstep(0.0, 1.0, vUv.x);
+
+  float line = smoothstep(0.0, 0.005, abs(vUv.y - 0.5));
+  float linearLine = smoothstep(0.0, 0.0075, abs(vUv.y - mix(0.5, 1.0, value1)));
+  float linearLine2 = smoothstep(0.0, 0.0075, abs(vUv.y - mix(0.0, 1.0, value1)));
+  float smoothLine = smoothstep(0.0, 0.0075, abs(vUv.y - mix(0.0, 0.5, value2)));
+
+  vec3 red = vec3(1.0, 0.0, 0.0);
+  vec3 blue = vec3(0.0, 0.0, 1.0);
+  vec3 white = vec3(1.0, 1.0, 1.0);
+
+  if (vUv.y > 0.5) {
+    colour = mix(red, blue, value1);
+  } else {
+    colour = mix(red, blue, value2);
+  }
+
+  colour = mix(white, colour, line);
+  colour = mix(white, colour, linearLine);
+  colour = mix(white, colour, linearLine2);
+  colour = mix(white, colour, smoothLine);
+
   gl_FragColor = vec4(colour, 1.0);
 }
+
+// void main() {
+//   vec3 colour = vec3(0.0);
+
+//   // colour = vec3(vUv.x);
+
+//   // Step
+//   // colour = vec3(step(0.5, vUv.x));
+
+//   // Mix 
+//   // mix(val1, val2, %)
+//   // mix(10., 20., 0.5); -> 15.
+//   // colour = mix(red, blue, vUv.x);
+
+//   // Smoothstep
+//   // smoothstep(edge0, edge1, x)
+//   // colour = vec3(smoothstep(0.0, 1.0, vUv.x));
+//   colour = mix(red, blue, smoothstep(0.0, 1.0, vUv.x));
+
+//   gl_FragColor = vec4(colour, 1.0);
+// }
--- js/Experience/shaders/section-4/vertex.glsl
+++ js/Experience/shaders/section-4/vertex.glsl
@@ -1,7 +1,7 @@
 
-varying vec2 vUvs;
+varying vec2 vUv;
 
 void main() {	
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  vUvs = uv;
+  vUv = uv;
 }
